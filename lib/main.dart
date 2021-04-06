810bac97302fef42b9cd1e4e6f85eb96924a3204
Venali Soni <sonivenali.vs@gmail.com>
2021-04-06 19:06:52 +0530

    Created Landing Screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tweet_dock/screens/landing_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+void main() {
+  runApp(TweetDock());
+}
+
+class TweetDock extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.ralewayTextTheme(textTheme)
+      ),
+      home: LandingScreen() ,
+    );
+  }
+}
---
+++ lib/screens/landing_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LandingScreen extends StatefulWidget {
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Connecting to the World...",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 14),
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(20)),
+                      color: Colors.blue,
+                      onPressed: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Create Account",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 18)),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+                bottom: 40,
+                child: Row(
+                  children: [
+                    Text(
+                      "Have an account already?",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    GestureDetector(
+                        onTap: () {},
+                        child: Text("  Log in",
+                            style: TextStyle(
+                                color: Colors.blueAccent, fontSize: 16)))
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
