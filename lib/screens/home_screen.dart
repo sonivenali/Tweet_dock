a9966d087f78dc1a45d88e9a210105c2c482b3d1
Venali Soni <sonivenali.vs@gmail.com>
2021-04-07 23:52:57 +0530

    Updated UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -41,32 +41,148 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Row(
               children: [
-                Expanded(flex: 1,
+                Expanded(
+                  flex: 1,
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 8,top: 8),
+                    padding: const EdgeInsets.only(left: 8, top: 8),
                     child: CircleAvatar(
-                      radius: 30.0,
+                      radius: 26.0,
                       backgroundImage: NetworkImage(
                           'https://static.remove.bg/remove-bg-web/71dbdf11b48cb655eefe2f609ad67295258ae141/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg'),
                     ),
                   ),
                 ),
-                Expanded(flex: 4,
+                Expanded(
+                  flex: 4,
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 8,right: 16),
+                    padding: const EdgeInsets.only(left: 8, right: 16),
                     child: TextFormField(
                       decoration: InputDecoration(
                         hintText: "What\'s happening?",
                         enabledBorder: UnderlineInputBorder(
                           borderSide: BorderSide(color: Colors.transparent),
                           //  when the TextFormField in unfocused
-                        ) ,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ],
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 60, top: 8),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: Icon(
+                      Icons.image,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: Icon(
+                      Icons.videocam,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: Icon(
+                      Icons.poll,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: Icon(
+                      Icons.emoji_emotions,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: Icon(
+                      Icons.schedule,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40),
+                    child: FlatButton(
+                        onPressed: () {},
+                        minWidth: 80,
+                        height: 45,
+                        color: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(32)),
+                        child: Text("Tweet",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ))),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: Divider(
+                height: 2,
+                thickness: 1,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 26.0,
+                    backgroundImage: NetworkImage(
+                        'https://pbs.twimg.com/profile_images/492035582734782464/7SbGRI_t_400x400.jpeg'),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8, top: 8),
+                            child: Text(
+                              "Kerry W. Lothrop",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8, left: 4),
+                            child: Text(
+                              "@kwlothrop . Mar 31",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "The float of shame. #evergiven",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                      Image.network(
+                        "https://pbs.twimg.com/media/ExqRrbOWEAgQloU?format=png&name=small",
+                        fit: BoxFit.cover,
+                        height: 200,width: MediaQuery.of(context).size.width-90,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
